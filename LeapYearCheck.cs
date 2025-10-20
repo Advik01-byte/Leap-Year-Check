1faa72e82492b1e1a9eebc282a3e7a47800e7a33
Advik01-byte <iamadvikgoyal@gmail.com>
2025-10-20 15:28:56 +0530

    Add files via upload

---
+++ LeapYearCheck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeapYearCheck
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.Write("\nPlease enter a year: ");
+      int year = Convert.ToInt32(Console.ReadLine());
+
+      if (!(year % 100 == 0))
+      {
+        if (year % 4 == 0)
+        {
+          Console.WriteLine("It is a leap year.\n");
+        }
+        else
+        {
+          Console.WriteLine("It is not a leap year.\n");
+        }
+      }
+      else
+      {
+        if (year % 400 == 0)
+        {
+          Console.WriteLine("It is a leap year.\n");
+        }
+        else
+        {
+          Console.WriteLine("It is not a leap year.\n");
+        }
+      }
+    }
+  }
+}
